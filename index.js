f5719a301b0efeefb07df801f09f0f143c141739
iqbal-mn694 <garenahiber134@gmail.com>
2024-07-19 21:33:02 +0700

    cors issues

--- index.js
+++ index.js
@@ -41,7 +41,9 @@ const app = express();
 // use middleware
 app.use(cors({
   origin: ["http://localhost:5500", "http://127.0.0.1:5500", "http://localhost:3000", "http://127.0.0.1:3000"],
-  credentials: true
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
 app.use(express.json())
