b18b1d7baf17e22f9a3a1a201f9c73bb323c8438
iqbal-mn694 <garenahiber134@gmail.com>
2024-08-01 17:21:03 +0700

    handle the donor quantity

--- controllers/donateController.js
+++ controllers/donateController.js
@@ -1,15 +1,16 @@
 const asyncWrapper = require("../libs/asyncWrapper");
 const db = require("../models/dbConnection")
 
-const { getBloodRequestByID, updateRequestStatus } = require("../models/BloodRequest");
+const { getBloodRequestByID, updateRequestStatus, updateFilledRequest } = require("../models/BloodRequest");
 const { countDonationByRequestID, insertDonation } = require("../models/Donation");
-const { insertDonor } = require("../models/Donor");
+const { insertDonor, getDetailDonor, getDetailDonorByRequestID, getDetailDonorByDonorID } = require("../models/Donor");
 
 
 // donate blood by request ID
 exports.donateBloodByRequestID = asyncWrapper(async (req, res) => {
   const { id: userID } = req.user;
   const { requestID } = req.params;
+  const { donorName, bloodType } = req.body;
 
   const donationTotalByRequestID = await countDonationByRequestID(requestID)
   const getBloodRequest = await getBloodRequestByID(userID, requestID)
@@ -17,19 +18,35 @@ exports.donateBloodByRequestID = asyncWrapper(async (req, res) => {
 
   // check if user's blood request is fulfiled
   if(donationTotalByRequestID <  bloodRequestQuantity) {
-    const donor = await insertDonor(userID);
-    const donation = await insertDonation(userID, requestID);
-
+    const donor = await insertDonor(userID, donorName, bloodType);
+    const updateFilled = await updateFilledRequest(requestID);
+    const donation = await insertDonation(userID, requestID, donor[0].id);
+    
     res.status(201).json({ success: true, status: 201, message: 'Blood has been donated successfully', data: donor });
   }
   // if fulfiled change status to fulfiled 
   const changeRequestStatus = await updateRequestStatus(requestID);
   res.status(200).json({ success: true, status: 200, message: 'Request status is fulfiled', data: changeRequestStatus })
-})
+});
+
+exports.detailDonorByRequestID = asyncWrapper(async (req, res) => {
+  const { requestID } = req.query;
+  const detailDonor = await getDetailDonorByRequestID(requestID);
+
+  res.status(200).json({ success: true, status: 200, message: 'Success get detail donor', data: detailDonor })
+});
+
+exports.detailDonorByDonorID = asyncWrapper(async (req, res) => {
+  const { donorID } = req.query;
+  const detailDonor = await getDetailDonorByDonorID(donorID);
+
+  res.status(200).json({ success: true, status: 200, message: 'Success get detail donor', data: detailDonor })
+});
 
 exports.donateBlood = asyncWrapper(async (req, res) => {
   const { id: userID } = req.userID;
-  const donor = await insertDonor(userID);
+  const { donorName, bloodType } = req.body;
+  const donor = await insertDonor(userID, donorName, bloodType);
   const donation = await insertDonation(userID);
 
   res.status(201).json({ success: true, status: 201, message: "Blood has been donated successfully" });
